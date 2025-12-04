185323ab4312aaec0a0dd05507c514b91f308f76
Constantine <84151879+Constantine-SRV@users.noreply.github.com>
2025-12-04 22:31:11 +0300

    Update ddl-sqlserver.sql

--- src/main/resources/benchmarks/twitter/ddl-sqlserver.sql
+++ src/main/resources/benchmarks/twitter/ddl-sqlserver.sql
@@ -6,7 +6,7 @@ IF OBJECT_ID('[tweets]') IS NOT NULL DROP table [dbo].[tweets];
 IF OBJECT_ID('[added_tweets]') IS NOT NULL DROP table [dbo].[added_tweets];
 IF OBJECT_ID('[user_profiles]') IS NOT NULL DROP table [dbo].[user_profiles];
 
--- Create Tables
+-- Create Tables with PAGE compression
 CREATE TABLE [dbo].[user_profiles] (
   uid int NOT NULL,
   name varchar(255) DEFAULT NULL,
@@ -14,20 +14,20 @@ CREATE TABLE [dbo].[user_profiles] (
   partitionid int DEFAULT NULL,
   partitionid2 tinyint DEFAULT NULL,
   followers int DEFAULT NULL,
-  PRIMARY KEY (uid)
-);
+  PRIMARY KEY (uid) WITH (DATA_COMPRESSION = PAGE)
+) WITH (DATA_COMPRESSION = PAGE);
 
 CREATE TABLE [dbo].[followers] (
   f1 int NOT NULL REFERENCES [user_profiles] (uid),
   f2 int NOT NULL REFERENCES [user_profiles] (uid),
-  PRIMARY KEY (f1,f2)
-);
+  PRIMARY KEY (f1,f2) WITH (DATA_COMPRESSION = PAGE)
+) WITH (DATA_COMPRESSION = PAGE);
 
 CREATE TABLE [dbo].[follows] (
   f1 int NOT NULL REFERENCES [user_profiles] (uid),
   f2 int NOT NULL REFERENCES [user_profiles] (uid),
-  PRIMARY KEY (f1,f2)
-);
+  PRIMARY KEY (f1,f2) WITH (DATA_COMPRESSION = PAGE)
+) WITH (DATA_COMPRESSION = PAGE);
 
 -- TODO: id AUTO_INCREMENT
 CREATE TABLE [dbo].[tweets] (
@@ -35,20 +35,20 @@ CREATE TABLE [dbo].[tweets] (
   uid int NOT NULL REFERENCES [user_profiles] (uid),
   text char(140) NOT NULL,
   createdate datetime DEFAULT NULL,
-  PRIMARY KEY (id)
-);
+  PRIMARY KEY (id) WITH (DATA_COMPRESSION = PAGE)
+) WITH (DATA_COMPRESSION = PAGE);
 
 CREATE TABLE [dbo].[added_tweets] (
   id bigint NOT NULL identity(1,1),
   uid int NOT NULL REFERENCES [user_profiles] (uid),
   text char(140) NOT NULL,
   createdate datetime DEFAULT NULL,
-  PRIMARY KEY (id)
-);
+  PRIMARY KEY (id) WITH (DATA_COMPRESSION = PAGE)
+) WITH (DATA_COMPRESSION = PAGE);
 
 
--- Create Indexes
-CREATE INDEX IDX_USER_FOLLOWERS ON [dbo].[user_profiles] (followers);
-CREATE INDEX IDX_USER_PARTITION ON [dbo].[user_profiles] (partitionid);
-CREATE INDEX IDX_TWEETS_UID ON [dbo].[tweets] (uid);
-CREATE INDEX IDX_ADDED_TWEETS_UID ON [dbo].[added_tweets] (uid);
+-- Create Indexes with PAGE compression
+CREATE INDEX IDX_USER_FOLLOWERS ON [dbo].[user_profiles] (followers) WITH (DATA_COMPRESSION = PAGE);
+CREATE INDEX IDX_USER_PARTITION ON [dbo].[user_profiles] (partitionid) WITH (DATA_COMPRESSION = PAGE);
+CREATE INDEX IDX_TWEETS_UID ON [dbo].[tweets] (uid) WITH (DATA_COMPRESSION = PAGE);
+CREATE INDEX IDX_ADDED_TWEETS_UID ON [dbo].[added_tweets] (uid) WITH (DATA_COMPRESSION = PAGE);
